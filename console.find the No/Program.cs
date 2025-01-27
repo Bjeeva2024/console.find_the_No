55a0c24e129d752c5f929596091b6a495bcbc3ca
jeeva <jeeva26dinesh16@gmail.com>
2025-01-27 18:39:01 +0530

    Add project files.

---
+++ console.find the No/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console.find_the_No
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playagain = true;
+            Console.WriteLine("Guess the NO");
+
+            while(playagain)
+            {
+                int numberToGuess = random.Next(1,11);
+                int guess = 0;
+                int attempt = 0;
+                Console.WriteLine("Guess the number 1 to 10");
+                while(guess != numberToGuess)
+                {
+
+                }
+
+            }
+        }
+    }
+}
